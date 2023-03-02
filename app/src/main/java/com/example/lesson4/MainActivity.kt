8d2fee8dc65c1fbb2de036459f14d9c55ad20b7c
sanjar221101 <sanjar212181@gmail.com>
2023-03-02 20:01:42 +0500

    initial commit

---
+++ app/src/main/java/com/example/lesson4/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.lesson4
+
+import android.content.IntentSender.OnFinished
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Switch
+import android.widget.TextView
+
+class MainActivity(millisInFuture: Long) : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        }
+
+
+
+            }
+
+            }
+
+
+        }
+}
+
+
+
